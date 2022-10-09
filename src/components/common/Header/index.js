96f01aa31beaff99039259f55dbade615efda2ad
namooplus <namoo.developer@gmail.com>
2022-10-10 00:41:09 +0900

    Header : Remove redundant codes

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
-const Header = (originalElement) => {
-  console.log(originalElement);
-
+const Header = () => {
   return (
     <Wrapper>
       <Title>River</Title>
