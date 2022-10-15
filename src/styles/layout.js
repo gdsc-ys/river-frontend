8a045fa84ce480c3eae152c6232dffcdd3a3d604
jennyjunho2 <a01032208149@gmail.com>
2022-10-15 14:33:40 +0900

    feat : add common style mixins

--- src/styles/layout.js
+++ src/styles/layout.js
@@ -4,3 +4,39 @@ export const fullscreen = css`
   width: 100%;
   min-height: 100vh;
 `;
+
+export const flexCenter = ({ horizontal }) => css`
+  // centering using block
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: ${horizontal || 'row'};
+`;
+
+export const absCenter = css`
+  // centering w/ utilize GPU
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const blockCenter = css`
+  //centering with block
+  display: block;
+  margin: 0 auto;
+`;
+
+export const textTrucate = css`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+export const pseudo = ({ display, position, content, width, height }) => css`
+  content: ${content || ''};
+  display: ${display || 'block'};
+  position: ${position || 'absolute'};
+  width: ${width || '100%'};
+  height: ${height || '100%'};
+`;
