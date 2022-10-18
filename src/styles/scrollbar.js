268b623ee04280824f0c2a6551f2babf76d5f221
jennyjunho2 <a01032208149@gmail.com>
2022-10-19 07:01:15 +0900

    Style : Add scrollbar styling

---
+++ src/styles/scrollbar.js
@@ -0,0 +1,21 @@
+import { css } from 'styled-components';
+
+export const customScrollbar = css`
+  &::-webkit-scrollbar {
+    width: 6px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 3px;
+    background-color: rgb(189, 189, 189);
+  }
+
+  &::-webkit-scrollbar-button {
+    width: 0;
+    height: 0;
+  }
+`;
