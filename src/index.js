05726d7e2940f4207e7c964822ef962c605ba18f
namooplus <namoo.developer@gmail.com>
2022-10-23 23:51:49 +0900

    Feat : Add normalize css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import App from 'App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'styles/global.css';
+import 'styles/normalize.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
