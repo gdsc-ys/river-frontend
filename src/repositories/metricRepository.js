2c46c7614d00f1a6745c34f77393da3fcaffcc21
jennyjunho2 <a01032208149@gmail.com>
2022-10-27 17:17:16 +0900

    Fix : fix typo

--- src/repositories/metricRepository.js
+++ src/repositories/metricRepository.js
@@ -2,7 +2,7 @@ import { get } from '@utils/fetcher';
 
 class MetricRepository {
   async getMetricHistory() {
-    return get('/ajax-api/2.0/preview/mlflow/metrics/get-historyt');
+    return get('/ajax-api/2.0/preview/mlflow/metrics/get-history');
   }
 }
 
