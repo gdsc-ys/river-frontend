b78fcd0e45f78cb5151a892987323c0143c94c53
jennyjunho2 <a01032208149@gmail.com>
2022-10-27 16:49:23 +0900

    Docs : Add useComponentVisible custom hook description

--- src/hooks/useComponentVisible.js
+++ src/hooks/useComponentVisible.js
@@ -1,5 +1,12 @@
 import { useEffect, useRef, useState } from 'react';
 
+/**
+ * @param {*} init initial state whether the component is visible or not.
+ * @returns {ref, isVisible, setIsVisible}
+ * ref : reference node, clicking outside of referenced node triggers handler
+ * isVisible : state whether element is visible.
+ * setIsVisible : Dispatcher that changes visibility state.
+ */
 const useComponentVisible = (init) => {
   const [isVisible, setIsVisible] = useState(init);
   const ref = useRef(null);
