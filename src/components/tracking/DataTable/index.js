0417601feca522e4ef020450d8327964466e6a8d
jennyjunho2 <a01032208149@gmail.com>
2022-11-04 10:28:16 +0900

    Feat : Parse time representation in table

--- src/components/tracking/DataTable/index.js
+++ src/components/tracking/DataTable/index.js
@@ -1,24 +1,42 @@
-import { Suspense, useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 import Table from '@components/common/Table';
 import { trackingService } from '@services/trackingService';
 import { isObjectEmpty } from '@utils/isObjectEmpty';
 
+dayjs.extend(relativeTime);
+
 const columnData = [
-  { Header: 'Created', accessor: 'info.start_time' },
+  {
+    Header: 'Created',
+    accessor: 'info.start_time',
+    Cell: (Info) => {
+      const { $y, $M, $D } = dayjs.unix(Info.cell.value / 1000);
+      return dayjs(`${$y}-${$M + 1}-${$D}`).fromNow();
+    },
+  },
   { Header: 'Last Runtime', accessor: 'info.end_time' },
 ];
 
 const DataTable = () => {
   const [runs, setRuns] = useState({});
-  const [loading, setLoading] = useState(true);
   const [isError, setIsError] = useState(false);
 
   const columns = useMemo(() => columnData, []);
 
   // fetch runs
-
   useEffect(() => {
+    const searchRun = async (experiment_ids) => {
+      await trackingService
+        .searchRun(experiment_ids)
+        .then((run) => {
+          setRuns(run.runs);
+        })
+        .catch((err) => console.log(err));
+    };
+
     const fetchRuns = async () => {
       await trackingService
         .getExperiments()
@@ -28,15 +46,7 @@ const DataTable = () => {
           for (let datum of data) {
             experiment_ids.push(datum.experiment_id);
           }
-          trackingService
-            .searchRun(experiment_ids)
-            .then((run) => {
-              setRuns(run.runs);
-            })
-            .catch((err) => console.log(err));
-        })
-        .then(() => {
-          setLoading(false);
+          searchRun(experiment_ids);
         })
         .catch((err) => console.log(err));
     };
