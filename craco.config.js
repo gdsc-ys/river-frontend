846b42e6f8264313b56a350ba80d614d8b2517a6
jennyjunho2 <a01032208149@gmail.com>
2022-11-04 13:42:57 +0900

    Initialization : Configure absolute import path autocomplete

--- craco.config.js
+++ craco.config.js
@@ -1,20 +1,13 @@
 /* eslint-disable no-undef */
-const path = require('path');
+const { CracoAliasPlugin } = require('react-app-alias');
+
+const options = {}; // default is empty for most cases
 
 module.exports = {
-  webpack: {
-    alias: {
-      '@': path.resolve(__dirname, 'src/'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@data': path.resolve(__dirname, 'src/data'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@repositories': path.resolve(__dirname, 'src/repositories'),
-      '@services': path.resolve(__dirname, 'src/services'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@interface': path.resolve(__dirname, 'src/interface'),
+  plugins: [
+    {
+      plugin: CracoAliasPlugin,
+      options: {},
     },
-  },
+  ],
 };
