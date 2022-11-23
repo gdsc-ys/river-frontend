f903e68d1fb869f1f5f4367bbb98ecd0a99706e5
jennyjunho2 <a01032208149@gmail.com>
2022-11-24 00:21:09 +0900

    Adding chart component...

--- src/components/common/Chart/Linechart/index.js
+++ src/components/common/Chart/Linechart/index.js
@@ -1,25 +1,79 @@
+import { useMemo } from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const mockData1 = [
-  {
-    x: '2018-03-01',
-    y: 30,
-  },
-  {
-    x: '2018-03-02',
-    y: 20,
-  },
-  {
-    x: '2018-03-03',
-    y: 40,
-  },
-];
+import {
+  AnimatedAxis,
+  AnimatedGrid,
+  AnimatedLineSeries,
+  XYChart,
+} from '@visx/xychart';
 
-const Linechart = ({ columns, data, width, height }) => {
-  return <ChartContainer width={width} height={height}></ChartContainer>;
+const Linechart = ({ data, Xaccessor, Yaccessor }) => {
+  const accessors = useMemo(() => {
+    return {
+      xAccessor: (d) => d.Xaccessor,
+      yAccessor: (d) => d.Yaccessor,
+    };
+  }, [data, Xaccessor, Yaccessor]);
+
+  const lineStyle = useMemo(() => {
+    return {
+      stroke: '#000000',
+      strokeLinecap: 'round',
+      strokeWidth: 1,
+    };
+  });
+
+  const TICK_LABEL_OFFSET = 10;
+
+  return (
+    <ChartContainer>
+      <XYChart>
+        <AnimatedGrid
+          columns={false}
+          numTicks={4}
+          lineStyle={lineStyle}
+          strokeDasharray="0, 4"
+        />
+        {/* X Axis */}
+        <AnimatedAxis
+          hideAxisLine
+          hideTicks
+          orientation="bottom"
+          tickLabelProps={() => {
+            // eslint-disable-next-line no-unused-labels
+            dy: TICK_LABEL_OFFSET;
+          }}
+        />
+        {/* Y Axis */}
+        <AnimatedAxis
+          hideAxisLine
+          hideTicks
+          orientation="left"
+          tickLabelProps={() => {
+            // eslint-disable-next-line no-unused-labels
+            dx: -10;
+          }}
+        />
+        <AnimatedLineSeries
+          stroke="#000000"
+          dataKey="line1"
+          data={data}
+          {...accessors}
+        />
+      </XYChart>
+    </ChartContainer>
+  );
 };
 
 export default Linechart;
+
+Linechart.propTypes = {
+  data: PropTypes.object.isRequired,
+  Xaccessor: PropTypes.string.isRequired,
+  Yaccessor: PropTypes.string.isRequired,
+};
 
 const ChartContainer = styled.div`
   width: ${(props) => props.width};
--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from '@/App';
+import Linechart from '@components/common/Chart/Linechart';
+import { mockData2 } from '@components/common/Chart/mockdata';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+// root.render(<App />);
+root.render(
+  <div>
+    <Linechart data={mockData2} Xaccessor="timestamp" Yaccessor="value" />
+  </div>,
+);
