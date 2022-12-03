dda0eab01489ca1787029fb7d145714dcb694e8d
namooplus <namoo.developer@gmail.com>
2022-12-03 20:22:25 +0900

    feat: add model entity

--- src/entities/common.ts
+++ src/entities/common.ts
@@ -3,6 +3,12 @@ export enum LifecycleStage {
   DELETED = 'deleted',
 }
 
+export enum ViewType {
+  ACTIVE_ONLY = 'ACTIVE_ONLY',
+  DELETED_ONLY = 'DELETED_ONLY',
+  ALL = 'ALL',
+}
+
 export interface Tag {
   key: string;
   value: string;
