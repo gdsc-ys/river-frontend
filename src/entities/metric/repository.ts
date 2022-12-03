ac7255e4717fac701b729ef9b5a83813cefa1108
namooplus <namoo.developer@gmail.com>
2022-12-03 21:39:09 +0900

    feat: define type on metric repository

---
+++ src/entities/metric/repository.ts
@@ -0,0 +1,11 @@
+import { Metric } from '@entities/metric';
+
+/**
+ * Get metric history
+ */
+export interface GetMetricHistoryRequest {
+  run_id: string; // ID of the run from which to fetch metric values. This field is required
+  metric_key: string; // Name of the metric. This field is required
+}
+
+export type GetMetricHistoryResponse = Metric[];
