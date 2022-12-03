d37704ebe31f78f2d93c3ee1d3061cbfd2d6becc
namooplus <namoo.developer@gmail.com>
2022-12-03 20:50:37 +0900

    feat: move to folder

--- src/entities/metric/index.ts
+++ src/entities/metric/index.ts
@@ -4,6 +4,7 @@
 export interface Metric {
   key: string; // Key identifying this metric
   value: number; // Value associated with this metric
-  timestamp: Date; // The timestamp at which this metric was recorded
   step: number; // Step at which to log the metric
+
+  timestamp: Date; // The timestamp at which this metric was recorded
 }
